3757454e55b8cd2dc604c4829103547194b0604a
jerald-raphel <jjeraldraphel@gmail.com>
2025-05-14 17:24:13 +0530

    Update CORS settings and server configurations

--- server.js
+++ server.js
@@ -61,3 +61,61 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const http = require('http');
+// const socketIo = require('socket.io');
+// require('dotenv').config();
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = socketIo(server, {
+//   cors: {
+//     origin: "*", // Allow all origins for Socket.IO
+//     methods: ["GET", "POST"]
+//   }
+// });
+
+// // Express CORS middleware
+// app.use(cors({
+//   origin: "*", // Allow all origins for Express
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: false
+// }));
+
+// app.use(express.json());
+
+// // Routes
+// app.use('/api', require('./routes/auth'));
+// app.use('/api/messages', require('./routes/messages'));
+
+// // Socket.IO Event Handling
+// let usersOnline = {};
+
+// io.on('connection', (socket) => {
+//   console.log('A user connected');
+
+//   socket.on('send_message', (data) => {
+//     const { senderName, receiverName, message } = data;
+//     io.emit('receive_message', { senderName, receiverName, message });
+//     console.log('Message sent from:', senderName, 'to:', receiverName);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected');
+//   });
+// });
+
+// // MongoDB Connection and Server Start
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log('MongoDB connected');
+//     server.listen(process.env.PORT || 10000, () =>
+//       console.log(`Server running on port ${process.env.PORT || 10000}`)
+//     );
+//   })
+//   .catch(err => console.error('MongoDB connection error:', err));
